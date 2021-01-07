f5063fe0894026c46413414f00601692c8e61b52
Jacob Walton <jawalton6616@gmail.com>
2021-01-07 13:27:54 -0600

    fixing deployment issues

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -4,7 +4,7 @@ import {
   KeyboardArrowUpRounded,
 } from "@material-ui/icons";
 import styles from "./table.module.css";
-import Link from "next/Link";
+import Link from "next/link";
 
 const orderBy = (countries, value, direction) => {
   if (direction == "asc") {
